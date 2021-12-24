30458c0f22f12fe712719d9a336e0fb93c57661e
Victor Arriaza <varriaza.work@gmail.com>
2021-12-23 16:16:54 -0800

    Created files to create time periods

--- create_time_price.py
+++ create_time_price.py
@@ -1,1 +1,12 @@
 # Create the time price periods used for strategies here. Then import them for use elsewhere.
+
+# Create a new csv file for each new time period.
+# Aka bear_to_bear would be its own file.
+# bull_to_bear would be another csv file.
+
+# Run specific functions in this file to create the csv's
+
+def create_testing_time_period():
+    pass
+    # read csv in as df
+    # read part of a csv in as a df?
