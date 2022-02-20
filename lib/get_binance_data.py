05fdd1f6f645787bef6c7182b0cba2584ee66e14
Victor Arriaza <varriaza@udel.edu>
2022-02-20 15:56:28 -0800

    collect binance data as a dataframe

--- lib/get_binance_data.py
+++ lib/get_binance_data.py
@@ -2,8 +2,10 @@
 # imports
 import time
 import datetime
+import pandas as pd
 from binance import AsyncClient, Client
 import lib.api_data as api # for Binance data
+from fractions import Fraction as frac
 
 def get_binance_data(TRADING_PAIR='ETHUSDT', start_date='', end_date=''):
     # If we get a date, turn it to a timestamp, otherwise just continue
@@ -42,8 +44,37 @@ def get_binance_data(TRADING_PAIR='ETHUSDT', start_date='', end_date=''):
             start_str=start_date,
             end_str=end_date
         )
-    # Print some of the data for reference
-    print(f'Historical values found: {len(klines)}')
-    print(f'Subset of data:\n{klines[0:5]}')
 
-    return klines
+    # Turn the list of lists into a dataframe
+    df_klines = pd.DataFrame(klines)
+    # Make fraction_price column
+    df_klines['fraction_price'] = (
+        df_klines[1].astype(float)+
+        df_klines[2].astype(float)+
+        df_klines[3].astype(float)+
+        df_klines[4].astype(float)
+    )/4
+    # Make decimal_price column before we turn the fraction_price into a fration
+    df_klines['decimal_price'] = round(df_klines['fraction_price'], 4)
+    # Make it a fraction type
+    df_klines['fraction_price'] = df_klines['fraction_price'].apply(frac)
+    # Create index column
+    df_klines.index.name = 'index'
+    # df_klines['index'] = df_klines.index
+
+    # Rename the first column to timestamp
+    column_names = df_klines.columns.tolist()
+    column_names[0] = 'timestamp'
+    df_klines.columns = column_names
+
+    # Turn timestamp column units into seconds
+    df_klines['timestamp'] = (df_klines['timestamp']/1000).astype(int)
+
+    # Reduce results to only the columns we want.
+    df_klines = df_klines.filter(['index', 'timestamp', 'fraction_price', 'decimal_price'])
+
+    # Print some of the data for reference
+    print(f'Historical values found: {len(df_klines.index)}')
+    print(f'Subset of data:\n{df_klines.head(5)}')
+
+    return df_klines
--- tests/test_get_binance_data.py
+++ tests/test_get_binance_data.py
@@ -10,29 +10,28 @@ def test_connection_and_results():
     Test that we can connect at all and get data back in roughly the format we want
     """
     binance_data = get_binance_data(start_date='2018-01-01-00-00-00', end_date='2018-01-01-01-00-00')
-    # assert we have enough expected rows in our list of lists
-    assert len(binance_data) > 0
-    # make sure we have data in the first list, we only use the first 5 values
-    assert len(binance_data[0]) > 4
+    # assert we get data
+    assert not binance_data.empty
 
 def test_data():
     """
     Verify that we get the expected amount of data from a repeated query
     """
     binance_data = get_binance_data(start_date='2018-01-01-00-00-00', end_date='2018-01-01-05-00-00')
-    # assert columns are expected
-    # index,timestamp,fraction_price,decimal_price
-    # [1514793600000, '751.77000000', '751.77000000', '750.01000000', '750.01000000', ...]
-    expected_timestamp = 1514793600000
-    expected_fraction_price = frac((751.77000000 + 751.77000000 + 750.01000000 + 750.01000000)/4)
-    assert binance_data[0][0] == expected_timestamp
-    binance_price = frac(
-        float(binance_data[0][1])+
-        float(binance_data[0][2])+
-        float(binance_data[0][3])+
-        float(binance_data[0][4])
-    )/4
-    assert binance_price == expected_fraction_price
+    # Limit data to first 3 returned values for simplicity
+    binance_data = binance_data.head(3)
+    
+    # assert column names are expected
+    assert binance_data.columns.tolist() == ['timestamp', 'fraction_price', 'decimal_price']
+    assert binance_data.index.name == 'index'
+    # assert data is expected
+    assert binance_data['timestamp'].tolist() == [1514793600, 1514793660, 1514793720]
+    assert binance_data['fraction_price'].tolist() == [
+        frac('3302449144722883/4398046511104'),
+        frac('6598037336888443/8796093022208'),
+        frac('3298754785653555/4398046511104')
+    ]
+    assert binance_data['decimal_price'].tolist() == [750.890, 750.110, 750.050]
 
 if __name__ == "__main__":
     pt.main(['tests/test_get_binance_data.py'])
